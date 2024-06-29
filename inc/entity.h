a6d270ec52c400fa5a4ede5cca3ccf277b7d4110
t0m45DEV <tomaslisazo12@gmail.com>
2024-06-29 11:18:15 -0300

    Cleaner entity code

--- inc/entity.h
+++ inc/entity.h
@@ -5,6 +5,13 @@
 #include "map.h"
 #include "trigonometry.h"
 
+#define FRONT_X_AXIS_COLLISION 1
+#define FRONT_Y_AXIS_COLLISION 2
+#define BACK_X_AXIS_COLLISION -1
+#define BACK_Y_AXIS_COLLISION -2
+#define NO_COLLISION 0
+
+
 /*
     An entity is every interactive and moving thing in the game, including the player
 
@@ -27,17 +34,19 @@ typedef struct entity_s {
 
 /*
     Returns the same entity with the x_offset and y_offset calculated
+    using the entity angle
 
     @param entity An entity type, the engine asumes that delta have correct values
 */
 entity_t calculate_offset(entity_t entity);
 
 
-#define FRONT_X_AXIS_COLLISION 1
-#define FRONT_Y_AXIS_COLLISION 2
-#define BACK_X_AXIS_COLLISION -1
-#define BACK_Y_AXIS_COLLISION -2
-#define NO_COLLISION 0
+/*
+    Returns true (not zero) if the thing at the position idx
+    can move towards the offset position
+*/
+int can_move(position_2D idx, position_2D offset);
+
 
 /*
     Given an entity and a collision position indicator, returns 0 if there is no collision in that direction, or another number of there is a collision
