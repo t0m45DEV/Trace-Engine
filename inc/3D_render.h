7f36ece1f49b0889d7d897c9d8b563bdfda08fc8
t0m45DEV <tomaslisazo12@gmail.com>
2024-11-05 19:44:02 -0300

    Corrected correction constants

--- inc/3D_render.h
+++ inc/3D_render.h
@@ -12,8 +12,8 @@
 
 #define DISTANCE_CORRECTION (((float) WINDOW_WIDTH) / ((float) WINDOW_HEIGHT)) /* To see the walls square */
 
-#define FLOOR_CORRECTION (WINDOW_WIDTH / 2) /* The floor (and also the ceiling) stops sliding with this one */
-#define CEILEING_CORRECTION (8 / resolution) /* The ceiling stops covering the walls */
+#define FLOOR_CORRECTION ((WINDOW_WIDTH / 2) - 4)  /* The floor (and also the ceiling) stops sliding with this one */
+#define CEILEING_CORRECTION (8 / resolution)       /* The ceiling stops covering the walls */
 
 /*
     Render a wall, off height (distance_from_player / MAX_WALL_HEIGHT) and draws it in the given ray position
