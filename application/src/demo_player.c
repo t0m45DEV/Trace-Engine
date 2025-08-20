01f414dcae4480cb6340ee6bd512c96dd5e7065f
t0m45DEV <tomaslisazo12@gmail.com>
2025-08-19 23:36:41 -0300

    Cleaner demo

--- application/src/demo_player.c
+++ application/src/demo_player.c
@@ -1,6 +1,8 @@
 #include "demo_player.h"
 
 #include "Tom_Engine.h"
+#include "trc_camera.h"
+#include "trigonometry.h"
 
 #define MOVE_FORWARD          'w'
 #define MOVE_BACKWARD         's'
@@ -12,8 +14,6 @@ const int rotation_velocity = 2;
 
 void move_demo_player(float delta_time)
 {
-    trc_world_position_t movement_dir = TRC_POS_ORIGIN;
-
     if (is_key_being_pressed(ROTATE_CLOCKWISE))
     {
         rotate_camera(rotation_velocity * delta_time);
@@ -25,13 +25,10 @@ void move_demo_player(float delta_time)
 
     if (is_key_being_pressed(MOVE_FORWARD))
     {
-        movement_dir = scalar_multiplication(get_camera_direction(), movement_velocity * delta_time);
-        move_camera(movement_dir);
+        move_camera(scalar_multiplication(get_camera_direction(), movement_velocity * delta_time));
     }
     if (is_key_being_pressed(MOVE_BACKWARD))
     {
-        movement_dir = scalar_multiplication(get_camera_direction(), movement_velocity * delta_time * (-1));
-        move_camera(movement_dir);
+        move_camera(scalar_multiplication(get_camera_direction(), movement_velocity * delta_time * (-1)));
     }
-    //move_camera(movement_dir);
 }
