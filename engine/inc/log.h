ad24d5d0dfad7abb723fc9abaaa08717ccafa500
t0m45DEV <tomaslisazo12@gmail.com>
2025-09-05 18:55:35 -0300

    log_debug inside LogMacros documentation group

--- engine/inc/log.h
+++ engine/inc/log.h
@@ -9,6 +9,7 @@
  */
 
 #ifndef GAME_EXPORT
+    /** @ingroup LogMacros */
     #define log_debug(...)   (log_message(_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__))
     #define log_info(...)    (log_message(_LOG_LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__))
 #else
