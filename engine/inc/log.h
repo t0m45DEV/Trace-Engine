292300e939287e12557b20e5b4fbcd0776f0e471
t0m45DEV <tomaslisazo12@gmail.com>
2025-09-05 18:38:15 -0300

    Documented log functions

--- engine/inc/log.h
+++ engine/inc/log.h
@@ -1,6 +1,13 @@
 #ifndef _H_LOG
 #define _H_LOG
 
+/** @defgroup LogMacros Logging Macros
+ *
+ * These macros prints the given message to the standard output, including the file and line of the caller.
+ *
+ * @{
+ */
+
 #ifndef GAME_EXPORT
     #define log_debug(...)   (log_message(_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__))
     #define log_info(...)    (log_message(_LOG_LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__))
@@ -12,6 +19,8 @@
 #define log_warning(...) (log_message(_LOG_LEVEL_WARNING, __FILE__, __LINE__, __VA_ARGS__))
 #define log_error(...)   (log_message(_LOG_LEVEL_ERROR, __FILE__, __LINE__, __VA_ARGS__))
 
+/** @} */
+
 typedef enum {
     _LOG_LEVEL_DEBUG,
     _LOG_LEVEL_INFO,
@@ -19,6 +28,14 @@ typedef enum {
     _LOG_LEVEL_ERROR,
 } log_levels_t;
 
+/**
+ * Prints to the standard output the given message formated with the given parameters.
+ *
+ * @param log_level The message importance level, this is used to include a keyword (DEBUG, INFO, WARNING and ERROR)
+ * @param file_name The file name of the caller
+ * @param line The line of the call
+ * @param message The text to print
+ */
 void log_message(const log_levels_t log_level, const char* file_name, const int line, const char* message, ...);
 
 #endif
