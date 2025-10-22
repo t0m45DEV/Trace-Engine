45b4f3fcde71a03393d52e738a867b00adb03fd0
t0m45DEV <tomaslisazo12@gmail.com>
2025-10-22 00:03:38 -0300

    File reader function added

---
+++ engine/inc/trc_file_manager.h
@@ -0,0 +1,8 @@
+
+/**
+ * Returns the content of the given file in the form of a string
+ *
+ * @note Caller MUST free the returned pointer
+ */
+const char* get_file_content(const char* filepath);
+
---
+++ engine/src/trc_file_manager.c
@@ -0,0 +1,41 @@
+#include "trc_file_manager.h"
+
+#include "log.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WRITE_MODE "wb"
+#define READ_MODE  "rb"
+
+const char* get_file_content(const char* filepath)
+{
+    FILE* file = fopen(filepath, READ_MODE);
+
+    if (file == NULL)
+    {
+        log_error("There was a error opening %s", filepath);
+        return NULL;
+    }
+    fseek(file, 0, SEEK_END);
+
+    int file_len = ftell(file);
+    rewind(file);
+
+    char* content = malloc(file_len + 1);
+
+    size_t read_size = fread(content, 1, file_len, file);
+
+    if (read_size == 0)
+    {
+        log_error("There was an error reading %s", filepath);
+        free(content);
+        return NULL;
+    }
+    content[file_len] = '\0';
+
+    fclose(file);
+
+    return (const char*) content;
+}
+
