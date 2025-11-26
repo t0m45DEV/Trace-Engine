6dbc36f31793464263d0e12eae822f5f40daf942
t0m45DEV <tomaslisazo12@gmail.com>
2025-11-26 11:54:33 -0300

    Added function for random RNG seed

--- engine/inc/trc_random.h
+++ engine/inc/trc_random.h
@@ -1,10 +1,21 @@
 
 /**
- * Sets the seed for the Random Number Generator
+ * Sets the seed for the Random Number Generator to the given
+ *  seed, use this instead of trc_set_RNG_random_seed if you
+ *  want control over what seed is being used (useful for
+ *  testing and replicate states)
  *
- * @note Run only once at the start of the program
+ * @note Run only ONCE at the start of the program
  */
-void trc_set_random_seed(int seed);
+void trc_set_RNG_seed(int seed);
+
+/***
+ * Sets the Random Number Generator a random seed, so
+ *  every time the game starts it generates new numbers
+ *
+ * @note Run only ONCE at the start of the program
+ */
+void trc_set_RNG_random_seed(void);
 
 /**
  * Returns a random integer between the given range (both values included)
--- engine/src/trc_random.c
+++ engine/src/trc_random.c
@@ -1,10 +1,16 @@
 #include "trc_random.h"
 
 #include <stdlib.h>
+#include <time.h>
 
-void trc_set_random_seed(int seed)
+void trc_set_RNG_seed(int seed)
 {
     srand(seed);
+}
+
+void trc_set_RNG_random_seed(void)
+{
+    trc_set_RNG_seed(time(NULL));
 }
 
 int trc_random_int(int min, int max)
